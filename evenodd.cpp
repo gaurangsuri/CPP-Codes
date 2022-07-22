d1c700e2176924fae3473c95a1a717dc47d01888
Gaurang Suri <96570794+gaurangsuri@users.noreply.github.com>
2022-07-22 12:30:20 +0530

    code to check if a number is odd or even

---
+++ evenodd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+std::cout;
+std::cin;
+std::endl;
+int main(){
+    int x;
+    cin>>x;
+    if(x%2==1){
+        cout<<"ODD";
+    }
+    else{
+        cout<<"EVEN";
+    }
+}
